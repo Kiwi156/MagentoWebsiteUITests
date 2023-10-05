fd917a21aac75317ea075263a37bfa3b89045219
Katie Beattie <KatieBeattie13@gmail.com>
2023-10-05 14:02:42 +1300

    Added more tests and page objects.

--- src/test/java/com/serenitydojo/ecommerce/acceptancetests/cart/DisplayingItemsInTheCartTest.java
+++ src/test/java/com/serenitydojo/ecommerce/acceptancetests/cart/DisplayingItemsInTheCartTest.java
@@ -8,9 +8,12 @@ import com.serenitydojo.ecommerce.pageobjects.ShoppingCartComponent;
 import net.serenitybdd.core.steps.UIInteractions;
 import net.serenitybdd.screenplay.ui.Button;
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
+import net.serenitybdd.screenplay.ui.Link;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -66,5 +69,26 @@ public class DisplayingItemsInTheCartTest extends UIInteractions {
 
         Assertions.assertThat(itemInTheCart).contains("Hero Hoodie", "Breathe-Easy Tank");
     }
+    @Test
+    void shouldBeAbleToRemoveAnItemFromTheCart() {
 
+        navigate.openTheApplication();
+        hotSellersComponent.selectItem("Breathe-Easy Tank", "L", "White");
+        productDetailsPage.addToCart();
+
+        shoppingCart.showCartSummary();
+
+        find(By.cssSelector("a.action.delete")).click();
+
+        find(By.cssSelector("button.action-primary.action-accept")).click();
+
+        shoppingCart.showCartSummary();
+
+        List<String> itemInTheCart = shoppingCart.getItems();
+
+        Assertions.assertThat(itemInTheCart).doesNotContain("Breathe-Easy Tank");
+
+
+
+    }
 }
