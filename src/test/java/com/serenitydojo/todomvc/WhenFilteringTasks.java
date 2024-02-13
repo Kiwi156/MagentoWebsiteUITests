c3c0c020db937ee3cdf249e7c9054d6a6716dbce
Katie Beattie <KatieBeattie13@gmail.com>
2024-02-13 14:26:59 +1300

    Completed parameterized test for multiple parameters using junit5 and a stream of arguments.

--- src/test/java/com/serenitydojo/todomvc/WhenFilteringTasks.java
+++ src/test/java/com/serenitydojo/todomvc/WhenFilteringTasks.java
@@ -1,61 +1,65 @@
 package com.serenitydojo.todomvc;
 
-
 import com.serenitydojo.todomvc.actions.TodoListActions;
 import net.serenitybdd.annotations.Managed;
 import net.serenitybdd.annotations.Steps;
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.openqa.selenium.WebDriver;
 import org.assertj.core.api.Assertions;
-import java.util.Collection;
 import java.util.List;
-
+import java.util.stream.Stream;
 import static java.util.Arrays.asList;
 
 @ExtendWith(SerenityJUnit5Extension.class)
 public class  WhenFilteringTasks {
 
     @Managed(driver = "chrome")
-    WebDriver driver;
 
     @Steps
     TodoListActions todoList;
 
-    private List<String> todoItems;
-    private String itemToComplete;
-    private String filterBy;
-    private List<String> filteredItem;
+    @ParameterizedTest(name = "{index} Should be able to search for {0} toDoItems by {2} filterBy Status")
+    @MethodSource
 
-    public WhenFilteringTasks(String filterBy, List<String> todoItems, String itemToComplete,  List<String> filteredItem) {
-        this.todoItems = todoItems;
-        this.itemToComplete = itemToComplete;
-        this.filterBy = filterBy;
-        this.filteredItem = filteredItem;
-    }
-
-
-    @ParameterizedTest(name =  "Filter By, Todo Items, Completed Items, Filtered Items")
-    public Collection<Object[]> testData() {
-        return asList(
-              new Object[][]{
-                      {"Active", asList("Feed the cat", "Walk the dog"), "Feed the cat", asList("Walk the dog")},
-                      {"Completed", asList("Feed the cat","Walk the dog"), "Feed the cat", asList("Feed the cat")},
-                      {"All", asList("Feed the cat", "Walk the dog", "Feed the cat"), asList("Feed the cat", "Walk the dog")},
-              }
-        );
-    }
-    @Test
-    public void shouldDisplayCorrectFilteredItemsTest(){
+    public void shouldDisplayCorrectFilteredItemsTest(List<String> toDoItems, String itemToComplete, String filterBy, List<String> filteredItem) {
         todoList.openApplication();
-        todoList.addItems(todoItems);
+        todoList.addItems(toDoItems);
         todoList.completeItem(itemToComplete);
-
         todoList.filterBy(filterBy);
 
         Assertions.assertThat(todoList.items()).containsExactlyElementsOf(filteredItem);
+
+    }
+    private static Stream<Arguments> shouldDisplayCorrectFilteredItemsTest() {
+        return Stream.of(Arguments.of(asList("Feed the cat", "Walk the dog"),"Feed the cat","Active", asList("Walk the dog")),
+                         Arguments.of(asList("Feed the cat", "Walk the dog"), "Feed the cat", "Completed",asList("Feed the cat")),
+                         Arguments.of(asList("Feed the cat", "Walk the dog"),"Walk the dog", "All",asList("Feed the cat", "Walk the dog")));
+
+
+    }
+
+
+    @Test
+    public void shouldDisplayCorrectActiveFilteredItemsTest() {
+        todoList.openApplication();
+        todoList.addItems("Feed the cat", "Walk the dog");
+        todoList.completeItem("Feed the cat");
+        todoList.filterBy("Active");
+
+        Assertions.assertThat(todoList.items()).containsExactly("Walk the dog");
+
+    }
+    @Test
+    public void shouldDisplayCorrectAllFilteredItemsTest() {
+        todoList.openApplication();
+        todoList.addItems("Feed the cat", "Walk the dog");
+        todoList.completeItem("Feed the cat");
+        todoList.filterBy("All");
+
+        Assertions.assertThat(todoList.items()).containsExactly("Feed the cat", "Walk the dog");
     }
 }
