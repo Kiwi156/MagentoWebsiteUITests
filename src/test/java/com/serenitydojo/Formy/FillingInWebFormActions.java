f60c083fd06d3bc6ce4a85f12da106af8f1ae255
Katie Beattie <KatieBeattie13@gmail.com>
2024-03-18 21:33:21 +1300

    Refactored Formy filling in web form action to pass in variables for education, gender and years of experience.

--- src/test/java/com/serenitydojo/Formy/FillingInWebFormActions.java
+++ src/test/java/com/serenitydojo/Formy/FillingInWebFormActions.java
@@ -1,41 +1,78 @@
 package com.serenitydojo.Formy;
 
+import net.serenitybdd.core.pages.WebElementFacade;
 import net.serenitybdd.core.steps.UIInteractions;
 import net.serenitybdd.annotations.Step;
 
 
-
 public class FillingInWebFormActions extends UIInteractions {
 
-    @Step("Fill in a new web form for {0} {1} {2}")
-    public void forANewCustomer(String FirstName,
-                                String LastName,
-                                String JobTitle){
+    @Step()
+    public void forANewCustomer(String firstName,
+                                String lastName,
+                                String jobTitle,
+                                String education,
+                                String sex,
+                                int yearsOfExperience) {
 
-    openUrl("https://formy-project.herokuapp.com/form");
+        openUrl("https://formy-project.herokuapp.com/form");
 
-    $("#first-name").type(FirstName);
+        $("#first-name").type(firstName);
 
-    $("#last-name").type(LastName);
+        $("#last-name").type(lastName);
 
-    $("#job-title").type(JobTitle);
+        $("#job-title").type(jobTitle);
 
-    $("#radio-button-2").click();
+        switch (education) {
+            case "High School" -> {
+                $("#radio-button-1").click();
+            }
+            case "College" -> {
+                $("#radio-button-2").click();
+            }
+            case "Grad School" -> {
+                $("#radio-button-3").click();
+            }
+            default -> {
+                throw new IllegalArgumentException();
+            }
+        }
 
-    $("#checkbox-2").click();
+        switch (sex) {
+            case "Male" -> {
+                $("#checkbox-1").click(); //Male selected
+            }
+            case "Female" -> {
+                $("#checkbox-2").click(); // Female selected
+            }
+            case "Prefer not to say" -> {
+                $("#checkbox-3").click(); // Prefer not to say selected
+            }
+            default -> {
+                throw new IllegalArgumentException();
+            }
+        }
 
-    $("[value='2']").click(); // 2-4 Years of Experience selected.
+        switch (yearsOfExperience) {
+            case 0, 1 -> {
+                $("[value='1']").click();
+            }
+            case 2, 3, 4 -> {
+                $("[value='2']").click();
+            }
+            case 5, 6, 7, 8, 9 -> {
+                $("[value='3']").click();
+            }
+            default-> {
+                $("[value='4']").click(); // 10 years plus experience
 
-    $("#datepicker").click(); // Current day will be selected.
+            }
+        }
 
+        $("#datepicker").click(); // Current day will be selected.
 
         $("[role='button']").click();
 
-
-
-
-
+        }
 
     }
-
-}
--- src/test/java/com/serenitydojo/Formy/acceptancetests/CompleteWebFormTests.java
+++ src/test/java/com/serenitydojo/Formy/acceptancetests/CompleteWebFormTests.java
@@ -3,23 +3,37 @@ package com.serenitydojo.Formy.acceptancetests;
 import com.serenitydojo.Formy.FillingInWebFormActions;
 import net.serenitybdd.annotations.Steps;
 import net.serenitybdd.core.steps.UIInteractions;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class CompleteWebFormTests extends UIInteractions{
+public class CompleteWebFormTests extends UIInteractions {
 
     @Steps
     FillingInWebFormActions fillingInWebForm;
 
-
     @Test
-    void shouldEnterDetailsAndSubmitForm(){
+    public void shouldEnterDetailsAndSubmitForm() {
+
 
         fillingInWebForm.forANewCustomer("Jo",
-                                         "Smith",
-                                         "Software Tester");
+                "Smith",
+                "Software Tester",
+                "College",
+                "Female",
+                2);
 
         waitForTextToAppear("The form was successfully submitted!");
     }
 
+    @Test
+    public void shouldEnterDetailsAndSubmitFormForPersonWithALongName() {
+
+        fillingInWebForm.forANewCustomer("Hubert",
+                "Wolfeschlegelsteinhausenbergerorff",
+                "Farmer",
+                "High School",
+                "Male",
+                11);
+
+        waitForTextToAppear("The form was successfully submitted!");
+    }
 }
